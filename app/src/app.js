c68dda93d57f185c35184b219c6f900bdf23a2d6
David Gilkeson <davidgilkeson8@gmail.com>
2020-06-17 16:54:05 +0100

    Initial commit
    Job Search app

---
+++ app/src/app.js
@@ -0,0 +1,13 @@
+//Main
+
+import {
+  JobSearch
+} from './JobSearch';
+
+const jobSearch = new JobSearch(
+  '#search-form',
+  '.result-container',
+  '.loading-element'
+);
+jobSearch.setCountryCode();
+jobSearch.configureFormListener();
---
+++ app/src/utils.js
@@ -0,0 +1,19 @@
+export const extractFormData = form => Array
+  .from(form.elements)
+  .reduce((acc, {
+    id,
+    value
+  }) => ({
+    [id]: value,
+    ...acc
+  }), {});
+
+export const getCurrencySymbol = country => {
+  const currencies = {
+    gb: '£',
+    us: '$',
+    au: '$',
+    ca: '$',
+  };
+  return currencies[country];
+}
